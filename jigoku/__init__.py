b0053cd5e525008a51b96b1ab0dee32f7f476a47
sinkaroid <bibsindra@gmail.com>
2023-02-20 06:46:56 +0700

    pre release

--- jigoku/__init__.py
+++ jigoku/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "2.2.2"
+__version__ = "2.2.3"
 from .app import main
 from .utils.log import log_data, log_time, get_hostname
 from .utils.disk import get_size, clean_html
-from .utils.scrape_posts import download_from_multiple_posts
-from .utils.scrape_pages import download_from_multiple_pages
+from .client.scrape_posts import download_from_multiple_posts
+from .client.scrape_pages import download_from_multiple_pages
